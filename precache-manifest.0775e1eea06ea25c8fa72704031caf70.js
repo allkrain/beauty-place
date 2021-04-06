9bed3660914df75dff3a65860985039eeff2f3ed
Allkrain <aukra25@yandex.ru>
2021-04-06 14:53:43 +0300

    deploy

---
+++ precache-manifest.0775e1eea06ea25c8fa72704031caf70.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58150611b3df7955b7f9",
+    "url": "/beauty-place/css/app.2b876230.css"
+  },
+  {
+    "revision": "137a9f5f920bcccad63c115397dc1532",
+    "url": "/beauty-place/fonts/Montserrat-Bold.137a9f5f.woff"
+  },
+  {
+    "revision": "1eaf59537a9c317acaa4b2c144cd89d6",
+    "url": "/beauty-place/fonts/Montserrat-Bold.1eaf5953.woff2"
+  },
+  {
+    "revision": "4193cb373574474bd262f0cada40c68e",
+    "url": "/beauty-place/fonts/Montserrat-Medium.4193cb37.woff2"
+  },
+  {
+    "revision": "72aebf4516ddcbc7634c4640fb6f9a90",
+    "url": "/beauty-place/fonts/Montserrat-Medium.72aebf45.woff"
+  },
+  {
+    "revision": "6c8807219b0ecffdf96122b80df3e62c",
+    "url": "/beauty-place/fonts/Montserrat-Regular.6c880721.woff2"
+  },
+  {
+    "revision": "c0b8804ae85213c9194816da945db150",
+    "url": "/beauty-place/fonts/Montserrat-Regular.c0b8804a.woff"
+  },
+  {
+    "revision": "22a59d9d14430bd96481dabfc29e3207",
+    "url": "/beauty-place/img/logo.22a59d9d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/beauty-place/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "67574fe5b2ca14c84702004af35aba2c",
+    "url": "/beauty-place/img/product1.67574fe5.jpg"
+  },
+  {
+    "revision": "30a5fb25ac2e6167f0c3ae18bc4b5e3e",
+    "url": "/beauty-place/img/product2.30a5fb25.jpg"
+  },
+  {
+    "revision": "cc43c5aeac613793a487b5ae25a1cf2b",
+    "url": "/beauty-place/img/product3.cc43c5ae.jpg"
+  },
+  {
+    "revision": "54a95d5329efea6a3aeca7c5c5a3efe2",
+    "url": "/beauty-place/img/product4.54a95d53.jpg"
+  },
+  {
+    "revision": "f19547e5330ea1a85c11dccc4840b149",
+    "url": "/beauty-place/img/product5.f19547e5.jpg"
+  },
+  {
+    "revision": "c6a086897687bb2dfb36d28ebea4eca0",
+    "url": "/beauty-place/index.html"
+  },
+  {
+    "revision": "58150611b3df7955b7f9",
+    "url": "/beauty-place/js/app.4a05d2bc.js"
+  },
+  {
+    "revision": "ff57747db054a343b9f4",
+    "url": "/beauty-place/js/chunk-vendors.f239d847.js"
+  },
+  {
+    "revision": "e5d8c74dfbd6497c1b80770f343fcefa",
+    "url": "/beauty-place/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/beauty-place/robots.txt"
+  }
+]);
